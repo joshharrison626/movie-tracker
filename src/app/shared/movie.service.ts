6c02d7404ee89c6accb238b57119db43557e41ce
Josh Harrison <joshua.j.harrison@intel.com>
2018-10-22 22:01:19 -0700

    feat(wip): add movie list and getter helper

--- src/app/shared/movie.service.ts
+++ src/app/shared/movie.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Movie } from '../models/movie.model';
+import { Observable } from 'rxjs';
+
+export const MOVIE_LIST: Movie[] = [
+  {
+    title: 'Jurassic Park',
+    genre: ['Science Fiction', 'Action', 'Adventure'],
+    rating: 5,
+  },
+  {
+    title: 'Stargate',
+    genre: ['Science Fiction', 'Action', 'Adventure'],
+    rating: 5,
+  },
+  {
+    title: 'Napoleon Dynamite',
+    genre: ['Comedy'],
+    rating: 5,
+  },
+];
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +26,8 @@ import { Injectable } from '@angular/core';
 export class MovieService {
 
   constructor() { }
+
+  getMovieList(): Promise<Movie[]> {
+    return Promise.resolve(MOVIE_LIST); // simulate async http request
+  }
 }
