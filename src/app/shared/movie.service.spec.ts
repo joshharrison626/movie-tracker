3e9f269ea11127cc34b2350f23da2755312cac62
Josh Harrison <joshua.j.harrison@intel.com>
2018-10-22 21:44:33 -0700

    refactor: make dependency injection easier

--- src/app/shared/movie.service.spec.ts
+++ src/app/shared/movie.service.spec.ts
@@ -1,15 +1,21 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 
 import { MovieService } from './movie.service';
 
 describe('MovieService', () => {
+  let service: MovieService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [MovieService]
+      providers: [
+        MovieService,
+      ]
     });
+
+    service = TestBed.get(MovieService);
   });
 
-  it('should be created', inject([MovieService], (service: MovieService) => {
+  it('should be created', () => {
     expect(service).toBeTruthy();
-  }));
+  });
 });
