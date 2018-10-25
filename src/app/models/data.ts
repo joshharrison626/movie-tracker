046a3d161c9c38b0c7ca79c3a0d9a7357a5afcd2
Harrison <joshua.j.harrison@intel.com>
2018-10-25 12:19:30 -0700

    refactor: rename file and add genre list

--- src/app/models/data.ts
+++ src/app/models/data.ts
@@ -1,22 +1,30 @@
-import { Movie } from '../models/movie.model';
+import { Movie } from './movie.model';
 
 export const MOVIE_LIST: Movie[] = [
   {
     id: Math.floor(Math.random() * 1000).toString(),
     title: 'Jurassic Park',
-    genre: ['Science Fiction', 'Action', 'Adventure'],
+    genre: 'Science Fiction',
     rating: '5',
   },
   {
     id: Math.floor(Math.random() * 1000).toString(),
     title: 'Stargate',
-    genre: ['Science Fiction', 'Action', 'Adventure'],
-    rating: '5',
+    genre: 'Science Fiction',
+    rating: '4',
   },
   {
     id: Math.floor(Math.random() * 1000).toString(),
     title: 'Napoleon Dynamite',
-    genre: ['Comedy'],
+    genre: 'Comedy',
     rating: '5',
   },
+];
+
+export const GENRE_LIST: string[] = [
+  'Action',
+  'Comedy',
+  'Fantasy',
+  'Horror',
+  'Science Fiction',
 ];
