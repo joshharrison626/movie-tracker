64c78aa72dd3bb90bfe79f0f766560d8bc0199c3
Harrison <joshua.j.harrison@intel.com>
2018-10-25 12:21:45 -0700

    feat(wip): add necessary material design modules

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,16 +2,43 @@ import { NgModule } from '@angular/core';
 import {
   MatButtonModule,
   MatCardModule,
+  MatListModule,
+  MatIconModule,
+  MatFormFieldModule,
+  MatSelectModule,
+  MatInputModule,
+  MatGridListModule,
+  MatToolbarModule,
+  MatTableModule,
+  MatSnackBarModule,
 } from '@angular/material';
 
 @NgModule({
   imports: [
     MatButtonModule,
     MatCardModule,
+    MatListModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatGridListModule,
+    MatTableModule,
+    MatSnackBarModule,
+    MatToolbarModule,
   ],
   exports: [
     MatButtonModule,
     MatCardModule,
+    MatListModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatTableModule,
+    MatSnackBarModule,
+    MatGridListModule,
+    MatToolbarModule,
   ],
 })
 export class MaterialModule { }
